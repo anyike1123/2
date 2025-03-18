11e0da74c6edea270936afaf1040bb023cded1ea
anyike1123 <87063407+anyike1123@users.noreply.github.com>
2025-03-18 16:58:20 +0800

    config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+define('API_URL', 'https://fx-api-testnet.gateio.ws/api/v4');
+define('API_KEY', '3bc13ae081eabd0f86399d84ae133c94');
+
+// 合约配置
+$contracts = ['BTC_USDT', 'ETH_USDT', 'SOL_USDT'];
+$timeframes = ['1m', '3m', '5m', '15m'];
